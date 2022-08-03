391cf5a538a64add3de0283e315fba0180f1f0a8
shinneng <shinneng@proximax.io>
2022-08-03 17:30:12 +0800

    update config for v0.9.0

--- docker-method/entrypoint.sh
+++ docker-method/entrypoint.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 if [[ -f "/data/server.lock" ]]; then
-        /sirius/bin/catapult.recovery /chainconfig
+    /sirius/bin/catapult.recovery /chainconfig
 fi
 
 if [[ -f "/data/recovery.lock" ]]; then
-    echo "unable to recover. resetting chain..."
-    echo "backing up nemesis block"
-    mv /data/00000/00001.dat /data/00000/hashes.dat /tmp
-    echo "deleting data directory"
-    for dir in /data/*; do
-        rm -rf $dir
-    done
-    mkdir /data/00000
-    mv /tmp/00001.dat /tmp/hashes.dat /data/00000/
-    if [ ! -f "/data/index.dat" ]; then
-                echo "No index.dat file, creating now...."
-                echo -ne "\01\0\0\0\0\0\0\0" > /data/index.dat
-    fi
-    echo "data has been reset to nemesis block"
+    echo "unable to recover corrupted chain data.  Run the restore.sh script"
 fi
 
 /sirius/bin/sirius.bc /chainconfig
