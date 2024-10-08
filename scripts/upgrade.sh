749136ed1022709d60acb61bdb33469f05940a71
angeljee <angeljee221001@gmail.com>
2024-10-08 15:04:59 +0800

    v1.9.1-update

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -4,7 +4,7 @@
 # VARS:
 DEFAULT_PATH="/mnt/siriuschain/"
 SERVICENAME="mainnet-peer"
-SIRIUS_CHAIN_VERSION="release-v1.9.0"
+SIRIUS_CHAIN_VERSION="release-v1.9.1"
 DOCKERFOLDER='docker-method'
 FAILMSG="UPGRADE FAIL"
 
